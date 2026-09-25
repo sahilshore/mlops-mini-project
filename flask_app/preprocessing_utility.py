f2ec6b38f9a51d32c9f694ac10c01583dba69aae
Sahil Shore <sahilkhan08052002@gmail.com>
2026-09-26 02:18:03 +0530

    add flask testing code

--- flask_app/preprocessing_utility.py
+++ flask_app/preprocessing_utility.py
@@ -1,24 +1,60 @@
-import unittest
-from flask_app.app import app
+import numpy as np
+import pandas as pd
+import os
+import re
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
-class FlaskAppTests(unittest.TestCase):
+def lemmatization(text):
+    """Lemmatize the text."""
+    lemmatizer = WordNetLemmatizer()
+    text = text.split()
+    text = [lemmatizer.lemmatize(word) for word in text]
+    return " ".join(text)
 
-    @classmethod
-    def setUpClass(cls):
-        cls.client = app.test_client()
+def remove_stop_words(text):
+    """Remove stop words from the text."""
+    stop_words = set(stopwords.words("english"))
+    text = [word for word in str(text).split() if word not in stop_words]
+    return " ".join(text)
 
-    def test_home_page(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'<title>Sentiment Analysis</title>', response.data)
+def removing_numbers(text):
+    """Remove numbers from the text."""
+    text = ''.join([char for char in text if not char.isdigit()])
+    return text
 
-    def test_predict_page(self):
-        response = self.client.post('/predict', data=dict(text="I love this!"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(
-            b'Happy' in response.data or b'Sad' in response.data,
-            "Response should contain either 'Happy' or 'Sad'"
-        )
+def lower_case(text):
+    """Convert text to lower case."""
+    text = text.split()
+    text = [word.lower() for word in text]
+    return " ".join(text)
 
-if __name__ == '__main__':
-    unittest.main()
+def removing_punctuations(text):
+    """Remove punctuations from the text."""
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+    text = text.replace('؛', "")
+    text = re.sub('\s+', ' ', text).strip()
+    return text
+
+def removing_urls(text):
+    """Remove URLs from the text."""
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    return url_pattern.sub(r'', text)
+
+def remove_small_sentences(df):
+    """Remove sentences with less than 3 words."""
+    for i in range(len(df)):
+        if len(df.text.iloc[i].split()) < 3:
+            df.text.iloc[i] = np.nan
+
+def normalize_text(text):
+    text = lower_case(text)
+    text = remove_stop_words(text)
+    text = removing_numbers(text)
+    text = removing_punctuations(text)
+    text = removing_urls(text)
+    text = lemmatization(text)
+
+    return text
