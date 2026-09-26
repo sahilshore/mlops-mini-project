8e8cdd4e907e960a0f76941295cc2e83bba38c78
Sahil Shore <sahilkhan08052002@gmail.com>
2026-09-27 04:05:18 +0530

    add aws ecr workflow to ci.yaml

--- flask_app/app.py
+++ flask_app/app.py
@@ -39,7 +39,7 @@ def removing_punctuations(text):
     """Remove punctuations from the text."""
     text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
     text = text.replace('؛', "")
-    text = re.sub('\s+', ' ', text).strip()
+    text = re.sub(r'\s+', ' ', text).strip()
     return text
 
 def removing_urls(text):
